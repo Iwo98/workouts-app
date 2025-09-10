5d60f7e35758ed03ee71187e9d110ea8cd43765e
iwobrzyski <iwo.brzyski98@gmail.com>
2025-09-10 20:24:04 +0200

    test2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import workoutsRouter from "./routes/workouts";
 const app = express();
 
 console.log("test");
+console.log("test1");
 
 app.use(express.json());
 app.use("/api/workouts", workoutsRouter);
