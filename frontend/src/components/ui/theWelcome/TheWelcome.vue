7656621b46a85b26308ba53a6a474719fcb81ff6
iwobrzyski <iwo.brzyski98@gmail.com>
2026-02-05 22:05:30 +0100

    added base coloring for the website

--- frontend/src/components/ui/theWelcome/TheWelcome.vue
+++ frontend/src/components/ui/theWelcome/TheWelcome.vue
@@ -6,7 +6,6 @@ defineProps<{
 
 <template>
   <div>
-    <h1 class="green">Your {{ msg }}</h1>
-    <h3 class="bg-amber-400">Let's get this party started</h3>
+    <h1>Your {{ msg }}</h1>
   </div>
 </template>
--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -9,9 +9,23 @@ const count = ref<number>(0);
 
 <template>
   <div>
-    <TheWelcome msg="Tutaj" />
-    <RouterLink to="/log-in">Log In</RouterLink>
+    <div class="p-2 flex gap-1 flex-col">
+      <TheWelcome msg="Tutaj" />
+      <TheWelcome class="bg-secondary rounded-xs" msg="Tutaj" />
+      <TheWelcome class="bg-secondary rounded-sms p-1" msg="Tutaj" />
+      <TheWelcome class="bg-secondary rounded-md" msg="Tutaj" />
+      <TheWelcome class="bg-secondary rounded-xl" msg="Tutaj" />
+      <TheWelcome class="bg-primary" msg="Tutaj" />
+      <TheWelcome class="bg-tertiary" msg="Tutaj" />
+      <RouterLink to="/log-in">Log In</RouterLink>
+    </div>
+    <Button as="button" @click="count++" variant="default">Button</Button>
+    <Button as="button" @click="count++" variant="destructive">Button</Button>
+    <Button as="button" @click="count++" variant="ghost">Button</Button>
+    <Button as="button" @click="count++" variant="link">Button</Button>
     <Button as="button" @click="count++" variant="outline">Button</Button>
-    <div>{{ count }}</div>
+    <Button as="button" @click="count++" variant="secondary">Button</Button>
+    <Button as="button" @click="count++" variant="secondary">Button</Button>
+    <div class="rounded-xl w-4">{{ count }}</div>
   </div>
 </template>
