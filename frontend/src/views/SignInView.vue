3b5a7f51339ed72b1746add5a238afd9c693f189
iwobrzyski <iwo.brzyski98@gmail.com>
2026-02-06 17:12:06 +0100

    login view

--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -3,29 +3,72 @@ import TheWelcome from "@frontend/components/ui/theWelcome/TheWelcome.vue";
 import { RouterLink, RouterView } from "vue-router";
 import { Button } from "@frontend/components/ui/button";
 import { ref } from "vue";
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as z from "zod";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@frontend/components/ui/form";
 
-const count = ref<number>(0);
+const formSchema = toTypedSchema(
+  z.object({
+    username: z.string().min(2).max(50),
+    password: z.string().min(2).max(50),
+  }),
+);
+const form = useForm({
+  validationSchema: formSchema,
+});
+
+const onSubmit = form.handleSubmit((values) => {
+  // oxlint-disable-next-line no-console
+  console.log("Form submitted!", values);
+});
 </script>
 
 <template>
-  <div>
-    <div class="p-2 flex gap-1 flex-col">
-      <TheWelcome msg="Tutaj" />
-      <TheWelcome class="bg-secondary rounded-xs" msg="Tutaj" />
-      <TheWelcome class="bg-secondary rounded-sms p-1" msg="Tutaj" />
-      <TheWelcome class="bg-secondary rounded-md" msg="Tutaj" />
-      <TheWelcome class="bg-secondary rounded-xl" msg="Tutaj" />
-      <TheWelcome class="bg-primary" msg="Tutaj" />
-      <TheWelcome class="bg-tertiary" msg="Tutaj" />
-      <RouterLink to="/log-in">Log In</RouterLink>
+  <div class="flex gap-3 flex-col items-start p-4">
+    <h1 class="fill-primary">Log in</h1>
+    <div class="w-full">
+      <form @submit="onSubmit">
+        <FormField v-slot="{ field }" name="username">
+          <FormItem class="w-full gap-1 mb-2">
+            <FormLabel>Username</FormLabel>
+            <FormControl class="rounded-xs border-2 border-tertiary w-full">
+              <Input
+                required
+                class="p-1"
+                placeholder="Username"
+                v-bind="field"
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <FormField name="password" v-slot="{ field }">
+          <FormItem class="w-full gap-1">
+            <FormLabel>Password</FormLabel>
+            <FormControl class="rounded-xs border-2 border-tertiary w-full">
+              <Input
+                type="password"
+                required
+                class="p-1"
+                placeholder="Your password"
+                v-bind="field"
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <Button as="button" type="submit" size="default" class="w-full mt-2"
+          >Log in</Button
+        >
+      </Form>
     </div>
-    <Button as="button" @click="count++" variant="default">Button</Button>
-    <Button as="button" @click="count++" variant="destructive">Button</Button>
-    <Button as="button" @click="count++" variant="ghost">Button</Button>
-    <Button as="button" @click="count++" variant="link">Button</Button>
-    <Button as="button" @click="count++" variant="outline">Button</Button>
-    <Button as="button" @click="count++" variant="secondary">Button</Button>
-    <Button as="button" @click="count++" variant="secondary">Button</Button>
-    <div class="rounded-xl w-4">{{ count }}</div>
   </div>
 </template>
